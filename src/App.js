3a3a722cce8116a75b2993fb89330f1ccb6af6dd
Arfa <iramtechy513@gmail.com>
2023-03-08 16:58:55 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react'
+import './components/css/Style.css';
 import './App.css';
-import Covid from './components/Covid';
+import axios from "axios";
+import CitySearch from './components/CitySearch';
+import WeatherCard from './components/WeatherCard';
+
 
 function App() {
+  const [weatherResult,setweatherResult]=useState(null)
+  
+  const onSearchSubmit = async (searchInputValue) => {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=ca261c971d5638db9d4d6cbccc1f093d`)
+    console.log(response.data)
+    setweatherResult(response.data)
+    console.log(weatherResult)
+ }
   return (
-    <div className='App' >
-      <Covid />
-    </div>
+   
+     <div className="container my-5">
+                <h1 className="text-center title">Weather in</h1>
+                 <CitySearch onSearchSubmit = {onSearchSubmit} />
+                 {weatherResult ?  <WeatherCard weatherResult = {weatherResult} /> : <div></div>}
+            </div>
+    
   )
 }
 
 export default App
-
